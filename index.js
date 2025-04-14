10f5d7fa5b90517a07290994d74a300c6e47381b
sooraj4sure <skashyap9711@gmail.com>
2025-04-14 14:51:51 +0530

    Backend ready

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function main() {
 
 app.use("/events", eventRoutes);
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Server running on port 5000");
 });
