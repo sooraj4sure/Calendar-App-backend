02b534af2dee23e8e65e2f6d6ac535b4c587ec66
sooraj4sure <skashyap9711@gmail.com>
2025-04-14 16:23:24 +0530

    db setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const eventRoutes = require("./routes/events");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -10,7 +11,11 @@ app.use(express.json());
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/calendarDB");
+  // await mongoose.connect("mongodb://127.0.0.1:27017/calendarDB");
+  // await mongoose.connect(`mongodb+srv://skashyap9711:surajkashyap@calendar.fbomphh.mongodb.net/?retryWrites=true&w=majority&appName=Calendar`);
+
+  // const MONGO_URI ="mongodb+srv://skashyap9711:surajkashyap@calendar.fbomphh.mongodb.net/?retryWrites=true&w=majority&appName=Calendar"
+  await mongoose.connect(process.env.MONGO_URI);
 
 }
 
